9d000ceef343dcddf909f35d827f2d0159f42831
seogi_macbook <sgkang09@github.com>
2016-02-04 13:48:22 -0800

    Add tests for convolution code (Time domain)

--- simpegAIP/TD/tests/analytictest_vmd.py
+++ simpegAIP/TD/tests/analytictest_vmd.py
@@ -1,16 +1,19 @@
+from SimPEG import Mesh, Survey, Problem, Utils, Models, Maps, PropMaps, np, sp, Solver as SimpegSolver
 from scipy.constants import mu_0
+from SimPEG.EM.Base import BaseEMProblem, EMPropMap
+from SimPEG.EM.TDEM.BaseTDEM import FieldsTDEM
+from SimPEG.Problem import BaseTimeProblem
 from simpegAIP.TD.BaseATEM import BaseATEMProblem
 from simpegAIP.TD import ProblemATEM_b
 import SimPEG.EM as EM
-from SimPEG import Mesh, np
 import EMTD
-from EMTD.Utils import hzAnalyticCentLoopT
+from EMTD.Utils import hzAnalyticDipoleT
 from pymatsolver import MumpsSolver
 import matplotlib.pyplot as plt
 
 import unittest
 
-def halfSpaceProblemAnaCircLoopDiff(showIt=False, waveformType="STEPOFF"):
+def halfSpaceProblemAnaVMDDiff(showIt=False, waveformType="STEPOFF"):
 	cs, ncx, ncz, npad = 20., 25, 25, 15
 	hx = [(cs,ncx), (cs,npad,1.3)]
 	hz = [(cs,npad,-1.3), (cs,ncz), (cs,npad,1.3)]
@@ -30,10 +33,10 @@ def halfSpaceProblemAnaCircLoopDiff(showIt=False, waveformType="STEPOFF"):
 		prb.t0 = time.min()
 	elif waveformType =="STEPOFF":
 		prb.timeSteps = [(1e-5, 10), (5e-5, 10), (1e-4, 10), (5e-4, 10), (1e-3, 10),(5e-3, 10)]
-
+	offset = 20.
 	tobs = np.logspace(-4, -2, 21)
-	rx = EM.TDEM.RxTDEM(np.array([[0., 0., 0.]]), tobs, "bz")
-	src = EM.TDEM.SrcTDEM_CircularLoop_MVP([rx], np.array([[0., 0., 0.]]), 13., waveformType)
+	rx = EM.TDEM.RxTDEM(np.array([[offset, 0., 0.]]), tobs, "bz")
+	src = EM.TDEM.SrcTDEM_VMD_MVP([rx], np.array([[0., 0., 0.]]), waveformType=waveformType)
 	survey = EM.TDEM.SurveyTDEM([src])
 	prb.Solver = MumpsSolver
 	sigma = np.ones(mesh.nC)*1e-8
@@ -43,13 +46,13 @@ def halfSpaceProblemAnaCircLoopDiff(showIt=False, waveformType="STEPOFF"):
 	prb.pair(survey)
 
 	out = survey.dpred(sigma)
-	bz_ana = mu_0*hzAnalyticCentLoopT(13., rx.times, sig_half)
+	bz_ana = mu_0*hzAnalyticDipoleT(offset, rx.times, sig_half)
 	err = np.linalg.norm(bz_ana-out)/np.linalg.norm(bz_ana)
 	print '>> Relative error = ', err
 
 	if showIt:
-		plt.loglog(rx.times, bz_ana)
-		plt.loglog(rx.times, out)
+		plt.loglog(rx.times, bz_ana, 'k')
+		plt.loglog(rx.times, out, 'b.')
 		plt.show()	
 	return err
 
@@ -57,10 +60,10 @@ def halfSpaceProblemAnaCircLoopDiff(showIt=False, waveformType="STEPOFF"):
 class TDEM_bTests(unittest.TestCase):
 
 	def test_analytic_STEPOFF(self):
-		self.assertTrue(halfSpaceProblemAnaCircLoopDiff(showIt=False,waveformType="STEPOFF") < 0.3)	
+		self.assertTrue(halfSpaceProblemAnaVMDDiff(showIt=True,waveformType="STEPOFF") < 0.3)	
 		
 	def test_analytic_GENERAL(self):
-		self.assertTrue(halfSpaceProblemAnaCircLoopDiff(showIt=False, waveformType="GENERAL") < 0.3)	        
+		self.assertTrue(halfSpaceProblemAnaVMDDiff(showIt=False, waveformType="GENERAL") < 0.3)	        
 
 if __name__ == '__main__':
     unittest.main()
